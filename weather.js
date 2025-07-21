f0857fa56c59c96616fe57bf776cd2ae2169d59b
G484142024 <248414@st.takushoku-u.ac.jp>
2025-07-21 20:05:48 +0900

    updated

--- weather.js
+++ weather.js
@@ -1,26 +1,24 @@
+function print(data) {
+  console.log("都市名: " + data.name);
+  console.log("経度: " + data.coord.lon);
+  console.log("緯度: " + data.coord.lat);
+  console.log("最低気温: " + data.main.temp_min + "°C");
+  console.log("最高気温: " + data.main.temp_max + "°C");
+  console.log("湿度: " + data.main.humidity + "%");
+  console.log("天気: " + data.weather[0].description);
+  console.log("風速: " + data.wind.speed + " m/s");
+  console.log("風向: " + data.wind.deg + "°");
 
-// 課題3-2 のプログラムはこの関数の中に記述すること
-function print() {
-  console.log(data.name);
-  console.log(data.coord.lon);
-  console.log(data.coord.lat);
-  console.log(data.main.temp_min);
-  console.log(data.main.temp_max);
-  console.log(data.main.humidity);
-  console.log(data.weather[0].description );
-  console.log(data.wind.speed);
-  console.log(data.wind.deg);
-
-  id = document.querySelector('input[name="kensaku"]');
-  console.log('市名ID：' + id.value );
+  //let id = document.querySelector('input[name="kensaku"]');
+  //console.log('市名ID：' + id.value );
 }
-let b = document.querySelector('button#print');
-b.addEventListener('click', print);
+
  
 // 課題5-1 の関数 printDom() はここに記述すること
 function printDom(data) {
 
   let div = document.querySelector('div#result');
+  div.innerHTML = '';
   let h2 = document.createElement('h2');
   h2.textContent = data.name;
   div.insertAdjacentElement('beforeend', h2 );
@@ -67,19 +65,44 @@ function printDom(data) {
 
 
 // 課題6-1 のイベントハンドラ登録処理は以下に記述
-
+let b = document.querySelector('button#find');
+b.addEventListener('click', sendRequest);
 
 
 
 // 課題6-1 のイベントハンドラ sendRequest() の定義
 function sendRequest() {
+  
+    // URL を設定
+    let cityid = document.querySelector('input[name="kensaku"]');
+    let idd = cityid.value
+    console.log(idd);
+    let url = 'https://www.nishita-lab.org/web-contents/jsons/openweather/'+idd+'.json';
+
+    // 通信開始
+    axios.get(url)
+        .then(showResult)   // 通信成功
+        .catch(showError)   // 通信失敗
+        .then(finish);      // 通信の最後の処理
 
 }
 
 // 課題6-1: 通信が成功した時の処理は以下に記述
 function showResult(resp) {
+  
+    // サーバから送られてきたデータを出力
+    let data = resp.data;
 
+    // data が文字列型なら，オブジェクトに変換する
+    if (typeof data === 'string') {
+        data = JSON.parse(data);
+    }
+
+    //print(data);
+    printDom(data);
 }
+
+
 
 // 課題6-1: 通信エラーが発生した時の処理
 function showError(err) {
@@ -142,8 +165,6 @@ let data = {
   "cod": 200
 };
 
-
-
-
+ 
  
 
